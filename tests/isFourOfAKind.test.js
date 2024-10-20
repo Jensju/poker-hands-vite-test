4c90441d75f9652479177ff78af722c31523d491
Jensju <jens@modulmedia.se>
2024-10-20 22:03:35 +0200

    Correct descriptions in four of a kind tests

--- tests/isFourOfAKind.test.js
+++ tests/isFourOfAKind.test.js
@@ -6,17 +6,17 @@ const suits = '♥♦♣♠';
 
 // Will fail since no code written in Compare Hands yet for the method
 // isThreeOfAKind
-test('Test that threeOfAKind returns truthy is three of a kind', () => {
+test('Test that fourOfAKind returns truthy when it is a four of a kind', () => {
   let hand = new Hand('♥7', '♦2', '♣7', '♠7', '♠7');
   expect(CompareHands.isFourOfAKind(hand)).toBeTruthy();
 });
 
-test('Test that threeOfAKind returns truthy is three of a kind', () => {
+test('Test that fourOfAKind returns falsy if there is not a four of a kind', () => {
   let hand = new Hand('♥7', '♦2', '♣8', '♠3', '♠7');
   expect(CompareHands.isFourOfAKind(hand)).toBeFalsy();
 });
 
-test('check that isThreeOfAKind returns a higher score for a stronger hand (if two hands but with three of a kind)', () => {
+test('check that isFourOfAKind returns a higher score for a stronger hand (if two hands both plays four of a kind)', () => {
   let hand1 = new Hand('♣2', '♣2', '♣2', '♣2', '♦4');
   let hand2 = new Hand('♦3', '♦3', '♦3', '♠3', '♦5');
   let hand1Score = CompareHands.isFourOfAKind(hand1);
